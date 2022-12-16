204a6a82048d88a69ed166bdb7518bc3b7e2b142
NataliiaPasaman <natapasaman@gmail.com>
2022-12-16 22:08:00 +0200

    create Navigation and add AppBar

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const UserMenu = () =>{
+
+    return (
+      <div>
+        <p>`Hello, user!`</p>
+        <button type="button">Log Out</button>
+      </div>
+    );
+}
