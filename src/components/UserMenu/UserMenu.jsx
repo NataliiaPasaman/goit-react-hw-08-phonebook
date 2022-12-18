39121b9610dc1bfca1f8e5b30ce6af04e31e9d0d
NataliiaPasaman <natapasaman@gmail.com>
2022-12-18 21:24:25 +0200

    fixed operations and add logOut

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { selectUser } from 'redux/auth/selectors';
+import { logOut } from 'redux/auth/operations';
 import { UserContainer, LogOutButton, UserTitle } from './UserMenu.styled';
 
 export const UserMenu = () =>{
+  const dispatch = useDispatch();
   const userName = useSelector(selectUser).name;
 
     return (
       <UserContainer>
         <UserTitle>{`Hello ${userName}!`}</UserTitle>
-        <LogOutButton type="button">Log Out</LogOutButton>
+        <LogOutButton type="button" 
+        onClick={() => dispatch(logOut())}
+        >
+          Log Out
+          </LogOutButton>
       </UserContainer>
     );
 }
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,12 +3,21 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+    axios.defaults.headers.common.Authorization = '';
+  };
+
 // Реєстрація нового користувача POST NEW USER
 export const register = createAsyncThunk(
     'auth/register',
     async (userData, thunkAPI) => {
         try {
             const response = await axios.post('/users/signup', userData);
+            setAuthHeader(response.data.token);
             return response.data;
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
@@ -23,7 +32,21 @@ export const logIn = createAsyncThunk(
     async (userData, thunkAPI) => {
         try {
             const response = await axios.post('/users/login', userData);
+            setAuthHeader(response.data.token);
             return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+);
+
+// Розлогінення користувача POST - Login Out User
+export const logOut = createAsyncThunk(
+    'auth/logout',
+    async (_, thunkAPI) => {
+        try {
+           await axios.post('/users/logout');
+           clearAuthHeader();
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
         }
