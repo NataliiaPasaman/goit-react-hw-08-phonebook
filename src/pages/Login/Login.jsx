dc8372ad8c281b1d3f3ab925c1af8996dd40f869
NataliiaPasaman <natapasaman@gmail.com>
2022-12-18 12:20:42 +0200

    add Login and Register forms

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,7 +4,15 @@ export const Login = () =>{
 
     return (
       <div>
-FormLogin
+        <form autoComplete="off">
+          <label>Email
+            <input type="email" name="email" />
+          </label>
+          <label>Password
+            <input type="password" name="password" />
+          </label>
+          <button type="submit">Log In</button>
+        </form>
       </div>
     );
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,7 +4,18 @@ export const Register = () =>{
 
     return (
       <div>
-Form
+        <form autoComplete="off">
+          <label>Username
+            <input type="text" name="name" />
+          </label>
+          <label>Email
+            <input type="email" name="email" />
+          </label>
+          <label>Password
+            <input type="password" name="password" />
+          </label>
+          <button type="submit">Register</button>
+        </form>
       </div>
     );
 }
