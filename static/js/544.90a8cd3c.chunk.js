8b567597d964e4b85c4e0dda48ca2d751c382ca3
NataliiaPasaman <103893175+NataliiaPasaman@users.noreply.github.com>
2022-12-25 21:19:37 +0000

    Deploying to gh-pages from @ NataliiaPasaman/goit-react-hw-08-phonebook@dfcad9007cebd6ab12830b975068923a7e8a0558 🚀

---
+++ static/js/544.90a8cd3c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{544:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});o(791);var t=o(329);function r(){return(0,t.jsx)("div",{children:(0,t.jsx)("p",{children:"Homepage"})})}}}]);
+//# sourceMappingURL=544.90a8cd3c.chunk.js.map
