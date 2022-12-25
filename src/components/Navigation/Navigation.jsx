e59d498febb840c4e6578fcdf0889bbab804b6e4
NataliiaPasaman <natapasaman@gmail.com>
2022-12-25 23:49:30 +0200

    fixed Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,14 @@
 import { StyledLink } from './Navigation.styled';
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "redux/auth/selectors";
 
 export const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
     
     return (
         <div>
         <StyledLink to="/">Home</StyledLink>
-        <StyledLink to="/contacts">Contacts</StyledLink>
+        {isLoggedIn && <StyledLink to="/contacts">Contacts</StyledLink>}
       </div>
     );
 }
