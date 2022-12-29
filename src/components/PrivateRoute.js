922e9ef1e6c0a10777aec889eac2c1b08ebd4132
NataliiaPasaman <natapasaman@gmail.com>
2022-12-29 22:59:05 +0200

    fixed Private Route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,9 @@
 import { Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn, selectIsRefreshing } from "redux/auth/selectors";
+import { selectIsLoggedIn } from "redux/auth/selectors";
 
 export const PrivateRoute = ({ children, redirectTo="/" }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  // const isRefreshing = useSelector(selectIsRefreshing);
-  // const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return isLoggedIn ? children : <Navigate to={redirectTo} />;
 };
